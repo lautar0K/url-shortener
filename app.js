385d00ca5457b67b05ac43181cefad5162ebb2db
lautar0K <laukpo@gmail.com>
2018-03-04 15:41:05 -0300

    Submitted favicon

--- app.js
+++ app.js
@@ -3,13 +3,19 @@ const app = express();
 const pg = require("pg");
 const valid = require("valid-url");
 
+let json = new Object();
+
 app.get("/:id", function(req, res, next) {
+  if(valid.isUri(req.params.id) == false) {
+    json.short = "Invalid URL";
+    break;
+  }
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
     if(err) {
       console.log("Error connecting.");
     }
-    client.query("INSERT INTO links(name, short) VALUES($1, $2) returning name",
-    [req.url, Math.round(Math.random * 10000)], function(err, result) {
+    client.query("INSERT INTO links(name, short) VALUES($1, $2)",
+    [req.params.id, Math.round(Math.random * 10000)], function(err, result) {
       done();
       if(err) {
         console.log("Error running query.", err);
