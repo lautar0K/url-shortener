35dda040fc78ff120019ccbbfcb6ba60b54b3d21
lautar0K <laukpo@gmail.com>
2018-03-04 15:47:39 -0300

    Added Valid URL check and JSON response

--- app.js
+++ app.js
@@ -8,21 +8,24 @@ let json = new Object();
 app.get("/:id", function(req, res, next) {
   if(valid.isUri(req.params.id) == false) {
     json.short = "Invalid URL";
-    break;
-  }
-  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    if(err) {
-      console.log("Error connecting.");
-    }
-    client.query("INSERT INTO links(name, short) VALUES($1, $2)",
-    [req.params.id, Math.round(Math.random * 10000)], function(err, result) {
-      done();
+  } else {
+    pg.connect(process.env.DATABASE_URL, function(err, client, done) {
       if(err) {
-        console.log("Error running query.", err);
+        console.log("Error connecting.");
       }
-      res.send(result);
+      let random = Math.round(Math.random * 10000);
+
+      client.query("INSERT INTO links(name, short) VALUES($1, $2)",
+      [req.params.id, random], function(err, result) {
+        done();
+        if(err) {
+          console.log("Error running query.", err);
+        }
+        json.short = random;
+        res.json(json);
+      })
     })
-  })
+  }
 });
 app.listen(process.env.PORT || 3000, function() {
   console.log("Listening on port ", this.address().port, app.settings.env)
