a803d1160a82a63fc7978899727d1f71ac14a42b
lautar0K <laukpo@gmail.com>
2018-03-05 19:30:35 -0300

    Added URL validator

--- app.js
+++ app.js
@@ -4,13 +4,14 @@ const pg = require("pg");
 const valid = require("valid-url");
 const sh = require("shorthash");
 
+let urlRegex = '^(?!mailto:)(?:(?:http|https|ftp)://)(?:\\S+(?::\\S*)?@)?(?:(?:(?:[1-9]\\d?|1\\d\\d|2[01]\\d|22[0-3])(?:\\.(?:1?\\d{1,2}|2[0-4]\\d|25[0-5])){2}(?:\\.(?:[0-9]\\d?|1\\d\\d|2[0-4]\\d|25[0-4]))|(?:(?:[a-z\\u00a1-\\uffff0-9]+-?)*[a-z\\u00a1-\\uffff0-9]+)(?:\\.(?:[a-z\\u00a1-\\uffff0-9]+-?)*[a-z\\u00a1-\\uffff0-9]+)*(?:\\.(?:[a-z\\u00a1-\\uffff]{2,})))|localhost)(?::\\d{2,5})?(?:(/|\\?|#)[^\\s]*)?$';
 let json = new Object();
 
 app.get("/:id", function(req, res, next) {
   let id = req.params.id;
-
   //Checks of the request is a valid URL
-  if (id != "favicon.ico") {
+  if (id.test(urlRegex) && id != "favicon.ico") {
+    console.log("Valid URL");
     pg.connect(process.env.DATABASE_URL, function(err, client, done) {
       if(err) {
         console.log("Error connecting.");
@@ -35,16 +36,6 @@ app.get("/:id", function(req, res, next) {
             json.original = id;
             json.short = hash;
             res.json(json);
-          })
-       //Handles short URL request and redirections to original URL
-        } else {
-          console.log("Already in DB.")
-          client.query("SELECT name FROM links WHERE short = '" + id + "'", function(err, result) {
-            done();
-            if(err) {
-              console.log("Error in query.", err);
-            }
-            res.status(301).redirect(id);
           })
         }
       })
