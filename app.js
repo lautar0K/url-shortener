aa0196123d0419562fb42212b88fb1d97f09df3e
lautar0K <laukpo@gmail.com>
2018-03-05 16:17:55 -0300

    Added add to DB function, logs

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ let json = new Object();
 app.get("/:id", function(req, res, next) {
   let id = req.params.id;
 
+  //Checks of the request is a valid URL
   if(valid.isUri(id) == false) {
     json.short = "Invalid URL";
   } else if (id != "favicon.ico") {
@@ -18,12 +19,15 @@ app.get("/:id", function(req, res, next) {
       }
       let hash = sh.unique(id);
 
+      //Checks if the request is already in DB
       client.query("SELECT FROM links WHERE name = '" + id + "'", function(err, result) {
         done();
         if(err) {
           console.log("Error in query.", err);
         }
+
         if(result.rowCount == 0) {
+          console.log("Added.")
           client.query("INSERT INTO links(name, short) VALUES($1, $2)",
           [id, hash], function(err, result) {
             done();
@@ -34,6 +38,9 @@ app.get("/:id", function(req, res, next) {
             json.short = hash;
             res.json(json);
           })
+        }
+        else {
+          console.log("Already in DB.")
         }
       })
     })
