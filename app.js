a5606ac8d296480fe59ab36cb6917855f2a5e6c8
lautar0K <laukpo@gmail.com>
2018-03-08 02:17:16 -0300

    Handled short requests

--- app.js
+++ app.js
@@ -16,11 +16,64 @@ app.get("/:id", function(req, res) {
 
   //Gets path
   let host = "https://fcc-url-shortnr.herokuapp.com/";
-  id = id.substr(host.length);
-  if(id.length > 0){
-    console.log(id, isUrl.test(id));
-  }
+  cleanId = id.substr(host.length);
+  console.log(id);
 
+  //Checks of the request is a valid URL
+  if(id > 0) {
+    if(!isUrl.test(id)) {
+      console.log(id + " Invalid.");
+
+      //Checks if the request is a short
+      pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+        if(err) {
+          console.log("Error connecting.");
+        }
+        let hash = sh.unique(id);
+
+        //Checks if the requested short is in DB
+        client.query("SELECT name FROM links WHERE short = '" + id + "'", function(err, result) {
+          done();
+          if(err) {
+            console.log("Error in query.", err);
+          }
+          console.log(result.toString());
+
+        })
+      });
+
+    } else {
+      if (id != "favicon.ico") {
+        pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+          if(err) {
+            console.log("Error connecting.");
+          }
+          let hash = sh.unique(id);
+
+          //Checks if the request is already in DB
+          client.query("SELECT FROM links WHERE name = '" + id + "'", function(err, result) {
+            done();
+            if(err) {
+              console.log("Error in query.", err);
+            }
+            //If it's not in DB it's added
+            if(result.rowCount == 0) {
+              console.log("Added.")
+              client.query("INSERT INTO links(name, short) VALUES($1, $2)",
+              [id, hash], function(err, result) {
+                done();
+                if(err) {
+                  console.log("Error running query.", err);
+                }
+                json.short = hash;
+              })
+            res.json(json);
+           }
+         })
+       })
+     }
+   }
+ }
 });
 app.listen(process.env.PORT || 3000, function() {
   console.log("Listening on port ", this.address().port, app.settings.env)
