22daa73c45e9fac00af402e2961d96697b0e3adb
lautar0K <laukpo@gmail.com>
2018-03-10 14:23:23 -0300

    Re-desing

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ let isUrl = /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\
 let json = new Object();
 
 app.get("/url/:id", function(req, res) {
-  console.log(req, req.originalUrl. req.path);
+  console.log(req);
 
 
  /*
